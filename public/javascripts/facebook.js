be46943eec15f251395e7bf5ab88881d967e49e3
unknown <dnharjani@gmail.com>
2013-08-10 13:41:42 +0100

    ported facebook functionality to requirejs

--- public/javascripts/facebook.js
+++ public/javascripts/facebook.js
@@ -1,82 +1,64 @@
-// Todo make this into a module
-
-var fbFriends, me;
-
-window.fbAsyncInit = function() {
-    // init the FB JS SDK
+define(["facebookSDK", "jquery", "underscore" ], function(FB, $, _){
     FB.init({
-        appId      : fbapikey,
-        channelUrl : '//localhost:8085/KIT/channel.html', // Channel File for x-domain communication
-        status     : true, // check the login status upon init?
-        cookie     : true, // set sessions cookies to allow your server to access the session?
-        xfbml      : true  // parse XFBML tags on this page?
+    appId      : 577325485612787,
+    channelUrl : '//localhost:3100/channel.html',
+    status     : true, // check the login status upon init?
+    cookie     : true, // set sessions cookies to allow your server to access the session?
+    xfbml      : true  // parse XFBML tags on this page?    
     });
 
-    FB.Event.subscribe('auth.statusChange', function(response) {
-        $('#fb-login-button').hide();
-        $('#logged-in-container').show();
-        $('#logged-in-container .has-tooltip').tooltip();
-
-        if (response.status === 'connected') {
-            FB.api('/me', {fields: 'name,id,location'} , function(result) {
-                me = result;
-                $(document).trigger('updatedMyInformation');
-            });
-        }
-    });
-};
-
-(function(d, debug){
-    var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-    if (d.getElementById(id)) {return;}
-js = d.createElement('script'); js.id = id; js.async = true;
-js.src = "//connect.facebook.net/en_US/all" + (debug ? "/debug" : "") + ".js";
-ref.parentNode.insertBefore(js, ref);
-}(document,true));
-
-$(document).ready(function(){
-    $(document.body).on("click", "#fb-login-button", function(){
+    function getLoginStatus(callback){
         FB.getLoginStatus(function(response) {
-            if (response.status === 'connected') {
+            callback(response);
+        });
+    }
 
-            } else {
-                fbLogin();
+    function getMyInfo(success, error){
+        FB.api('/me', {fields: 'name,id,location'} , function(result) {
+            if(result){
+                success(result);
+            }
+            else{
+                error();
+            }
+        }); 
+    }
+
+    function getFriendsInfo(success, error){
+        FB.api('/me/friends', {fields: 'name,id,location'} , function(result) {
+            if(result){
+                success(result);
+            }
+            else{
+                error();
             }
         });
-    });
+    }
 
-    $(document.body).on("click", "#fb-logout-button", function(){
-        fbLogout();
-    });
+    function login(success, error) {
+        FB.login(function(response) {
+            if (response.authResponse) {
+                success();
+            }
+            else {
+                error();
+            }
+        }, {scope:'user_location, friends_location'});
+    };
+
+    function logout(callback){
+        FB.logout(function(response) {
+            callback();     
+        });
+    };
+
+    var facebook = {
+        login : login,
+        logout : logout,
+        getMyInfo : getMyInfo,
+        getFriendsInfo : getFriendsInfo,
+        getLoginStatus : getLoginStatus
+    };
+
+    return facebook;
 });
-
-function fbLogin() {
-    FB.login(function(response) {
-        if (response.authResponse) {
-            $('#fb-login-button').hide();
-            $('#logged-in-container').show();
-            $('#logged-in-container .has-tooltip').tooltip();
-        }
-        else {
-            // cancelled
-        }
-    }, {scope:'user_location, friends_location'});
-}
-
-function fbLogout(){
-    FB.logout(function(response) {
-//        localStorage.removeItem('pw-'+me.id+'-friends');
-//        localStorage.removeItem('pw-'+me.id+'-locations');
-        $('#fb-login-button').show();
-        $('#logged-in-container').hide();
-        window.location.reload();
-    });
-}
-
-$(document).on('updatedMyInformation', function(){
-    FB.api('/me/friends', {fields: 'name,id,location'} , function(result) {
-        fbFriends = result.data;
-        $(document).trigger('updatedFriendLocations');
-    });
-});
-
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,8 +1,43 @@
+require.config({
+    paths: {
+        "jquery": "vendor/jquery",
+        "bootstrap": "vendor/bootstrap",
+        "gmaps": "vendor/gmaps",
+        "knockout" : "vendor/knockout",
+        "underscore" : "vendor/underscore",
+        "modernizr" : "vendor/modernizr",
+        "facebookSDK": "//connect.facebook.net/en_US/all",
 
-require(["libs/jquery-1.9.1.js", "facebook", "libs/modernizr", "google-maps", "libs/bootstrap"], function($) {
-    $(function() {
+        "appBindings" : "appBindings",
 
-    });
+    },
+    shim: {
+        bootstrap: {
+            deps: ["jquery"]
+        },
+
+        underscore: {
+            exports: '_'
+        },
+
+        gmaps: {
+            exports: 'GMaps'
+        },
+
+        modernizr: {
+            exports: 'Modernizr'
+        },
+
+        facebookSDK : {
+            exports: 'FB'
+        }
+
+    }
 });
 
-define("main", function(){});
+require(
+    ["jquery", "knockout", "appBindings", "facebook"],function($, ko, appModel, facebook){
+        appModel.initialize();
+        ko.applyBindings(appModel);
+    }
+);
