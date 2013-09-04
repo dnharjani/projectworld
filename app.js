7beef0cb85e3e75ea1299980ce2f9ce05e60ffd0
Danesh Harjani <danesh@dnharjani.com>
2013-09-04 15:17:10 +0100

    added config options for deploying to appfog

--- app.js
+++ app.js
@@ -20,8 +20,37 @@ fs.readdirSync(models_path).forEach(function (file) {
    }
 });
 
+
 // Connect to mongo
-mongoose.connect('mongodb://localhost:27017');
+if(process.env.VCAP_SERVICES){
+    var env = JSON.parse(process.env.VCAP_SERVICES);
+    var mongo = env['mongodb-1.8'][0]['credentials'];
+}
+else{
+    var mongo = {
+        "hostname":"localhost",
+        "port":27017,
+        "username":"",
+        "password":"",
+        "name":"",
+        "db":"project-world"
+    }
+}
+
+var generateMongoUrl = function(obj){
+  obj.hostname = (obj.hostname || 'localhost');
+  obj.port = (obj.port || 27017);
+  obj.db = (obj.db || 'test');
+  if(obj.username && obj.password){
+    return "mongodb://" + obj.username + ":" + obj.password + "@" + obj.hostname + ":" + obj.port + "/" + obj.db;
+  }
+  else{
+  return "mongodb://" + obj.hostname + ":" + obj.port + "/" + obj.db;
+}
+}
+var mongourl = generateMongoUrl(mongo);
+mongoose.connect(mongourl);
+
 
 // Start app
 var app = express();
--- public/javascripts/apikeys.js
+++ public/javascripts/apikeys.js
@@ -1,2 +1,3 @@
 // server fbapikey = 519658164743899;
 // local fbapikey = 577325485612787;
+// appfog fbapikey = 184964745016516
